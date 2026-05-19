035777e62793c5abfd3f27ff31d48adde9455e5b
yolothestark <yolothestark@gmail.com>
2026-05-19 16:59:47 -0600

    aa

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Directorio de ONGs</title>
     
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    
     
     @vite([
         'resources/css/app.scss', 
